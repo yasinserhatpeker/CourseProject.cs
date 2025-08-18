a8d360d86257940d46068431050ba8f83020abf6
Yasin <yasinpeker43@gmail.com>
2025-08-18 19:36:54 +0300

    Course and Student tables are joined in CourseRegister.

--- Data/CourseRegister.cs
+++ Data/CourseRegister.cs
@@ -8,7 +8,11 @@ public class CourseRegister
     public int RegisterId { get; set; }
     public int StudentId { get; set; }
 
+    public Student Student { get; set; } = null!;
+
     public int CourseId { get; set; }
+
+    public Course Course { get; set; } = null!;
 
     public DateTime RegisterTime { get; set; }
 
