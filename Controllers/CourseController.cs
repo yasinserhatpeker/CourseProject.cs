f604f880003d7a324b123730a7c159783b21b382
Yasin <yasinpeker43@gmail.com>
2025-08-18 10:31:31 +0300

    Configured CourseController and its views.

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -6,6 +6,24 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 public class CourseController : Controller
 {
-    
-    
+    private readonly DataContext _context;
+
+    public CourseController(DataContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Course model)
+    {
+        _context.Courses.Add(model);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+}
+
 }
