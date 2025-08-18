f42dbf62192f8b576d938956eaae284a9aa26ba6
Yasin <yasinpeker43@gmail.com>
2025-08-18 09:59:53 +0300

    CourseController.cs is created.

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,11 @@
+namespace efCore.Controllers;
+using efCore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+public class CourseController : Controller
+{
+    
+    
+}
--- obj/Debug/net8.0/efCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/efCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69949deb5aba011a5e17a4e692ceeeb1d29c147c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd45866182a363c421366cb308412b490aaced8")]
 [assembly: System.Reflection.AssemblyProductAttribute("efCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
