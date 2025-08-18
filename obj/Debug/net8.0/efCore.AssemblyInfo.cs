8aae1889232628937b4701fa113e6af1906391f4
Yasin <yasinpeker43@gmail.com>
2025-08-18 15:24:13 +0300

    /Course/Delete.cshtml is created.

--- obj/Debug/net8.0/efCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/efCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd94199f3f25eccf4ab468c81cb74ed859be64a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f13de72f76b4f7eb54c2a33c1443e2b084a4be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("efCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
