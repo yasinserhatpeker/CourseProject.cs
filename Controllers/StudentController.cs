54cbb31a70478a34ebcc6186d48a2912bf1341ff
Yasin <yasinpeker43@gmail.com>
2025-08-18 09:04:27 +0300

    Trainee update page is created.

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -13,7 +13,7 @@ public class StudentController : Controller
     }
     public async Task<IActionResult> List()
     {
-         return View( await _context.Students.ToListAsync());
+        return View(await _context.Students.ToListAsync());
     }
 
     [HttpGet]
@@ -30,6 +30,21 @@ public class StudentController : Controller
         await _context.SaveChangesAsync();
         return RedirectToAction("Index", "Home");
 
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var student = await _context.Students.FirstOrDefaultAsync(x => x.StudentId == id);
+
+        if (student == null)
+        {
+            return NotFound();
+        }
+        return View(student);
     }
 
     
--- obj/Debug/net8.0/efCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/efCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb6a7fb959b1d5a261de27b8fa9e255691e19db0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3abcfd10d2e2461337c660ab0dfa6c126811b2d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("efCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
