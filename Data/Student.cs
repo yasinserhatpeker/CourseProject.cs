c2dae4a5acbcfdb2dbb7fed841861dbe8bb8beed
Yasin <yasinpeker43@gmail.com>
2025-08-19 09:10:25 +0300

    Courses section is added to the student/edit.cshtml.

--- Data/Student.cs
+++ Data/Student.cs
@@ -10,14 +10,19 @@ public class Student
     public string? StudentName { get; set; }
 
     public string? StudentSurname { get; set; }
-    
-    public string NameSurname { get
+
+    public string NameSurname
+    {
+        get
         {
-           return this.StudentName + " " + this.StudentSurname;
-        } }
+            return this.StudentName + " " + this.StudentSurname;
+        }
+    }
     public string? StudentEmail { get; set; }
 
     public string? StudentPhone { get; set; }
+    
+    public ICollection<CourseRegister> CourseRegisters { get; set; } = new List<CourseRegister>();
     
 
 }
--- obj/Debug/net8.0/efCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/efCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d360d86257940d46068431050ba8f83020abf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea36843f15967d516fc0276d3802872c67fbddbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("efCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
