6d30f595565b9acbb521e30bf490a091557359c7
Yasin <yasinpeker43@gmail.com>
2025-08-19 14:50:28 +0300

    finalized the homepage.

--- obj/Debug/net8.0/efCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/efCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff86b941908403f0c199d0a4fe93759003bd11e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50fb43a4d80c3e0084699e31129105826ed9e6db")]
 [assembly: System.Reflection.AssemblyProductAttribute("efCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
