eff86b941908403f0c199d0a4fe93759003bd11e
Yasin <yasinpeker43@gmail.com>
2025-08-19 14:39:30 +0300

    changed home and privacy pages.

--- obj/Debug/net8.0/efCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/efCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5726ec8db2390317e68e7eb3af41bce777259eca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c196b45643692bb26541d413af5e95fa19cbde2")]
 [assembly: System.Reflection.AssemblyProductAttribute("efCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
